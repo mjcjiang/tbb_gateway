c8926c00932decd42a6bca4c39f933318f1337bb
mjcjiang <redmagic039@gmail.com>
2023-07-17 22:47:03 +0800

    big change

--- include/ctp_market.h
+++ include/ctp_market.h
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <string>
+#include <memory>
+#include "ThostFtdcMdApi.h"
 
-class Test {
- public:
-    Test(const std::string& name)
-        :name_(name) {
+class MdHandler : public CThostFtdcMdSpi {
+public:
+    //constructor, need a valid pointer to CThostFtdcMdApi
+    MdHandler(CThostFtdcMdApi *pMdApi) : m_Api(pMdApi) {}
+    ~MdHandler() {}
+
+    //after establish connection to NGTS, do user login
+    void OnFrontConnected() {
+        CThostFtdcReqUserLoginField login_field;
+        login_field.
+            //m_api->ReqUserLogin(CThostFtdcReqUserLoginField *pReqUserLoginField, int nRequestID) = 0;
     }
-
-    std::string get_name();
- private:
-    std::string name_;
+  private:
+    std::unique_ptr<CThostFtdcMdApi> m_Api;
+    int m_RequestId
 };
--- services/test_name.cc
+++
@@ -1,8 +0,0 @@
-#include "ctp_market.h"
-#include <iostream>
-
-int main(void) {
-    Test test("zhangjiang");
-    std::cout << test.get_name() << std::endl;
-    return 0;
-}
