7e5caa870b6c11fe2acaeb5c4e2965cb2e36152d
mjcjiang <redmagic039@gmail.com>
2023-07-18 16:37:38 +0800

    small change

--- apps/market_server.cc
+++ apps/market_server.cc
@@ -9,28 +9,31 @@ int main() {
     if (res) {
         SPDLOG_ERROR("Failed to fetch account.json");
     }
-
+    
     Semaphore &sem = Semaphore::GetInstance();
-    CThostFtdcMdApi *pUserMdApi = CThostFtdcMdApi::CreateFtdcMdApi("", false, false);
+    CThostFtdcMdApi *pUserMdApi =
+        CThostFtdcMdApi::CreateFtdcMdApi("", false, false);
     MdHandler md_handler(pUserMdApi);
 
     pUserMdApi->RegisterSpi(&md_handler);
     pUserMdApi->RegisterFront(const_cast<char *>(acct_info.md_uri.c_str()));
     SPDLOG_INFO("RegisterFront {}", acct_info.md_uri);
     pUserMdApi->Init();
-   
-    //wait for signal
+    
+    // wait for signal
     SPDLOG_INFO("Wait front connection established...");
     sem.Wait();
-    
+
     SPDLOG_INFO("Start login...");
-    CThostFtdcReqUserLoginField reqUserLogin = { 0 };
-    memcpy(reqUserLogin.BrokerID, acct_info.broker_id.c_str(), acct_info.broker_id.size()); 
+    CThostFtdcReqUserLoginField reqUserLogin = {0};
+    memcpy(reqUserLogin.BrokerID, acct_info.broker_id.c_str(),
+           acct_info.broker_id.size());
     pUserMdApi->ReqUserLogin(&reqUserLogin, 2);
 
     sem.Wait();
     SPDLOG_INFO("Finish login...");
-    //TODO: start to listen on some port, use zero mq, process user login and subscribe
-    
+    // TODO: start to listen on some port, use zero mq, process user login and
+    // subscribe
+  
     return 0;
 }
