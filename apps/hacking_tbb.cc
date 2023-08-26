29ab18df88c126ae5958b0d67b6c7d94aee25033
mjcjiang <redmagic039@gmail.com>
2023-08-26 22:29:46 +0800

    code commit

--- apps/hacking_tbb.cc
+++ apps/hacking_tbb.cc
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <tbb/tbb.h>
+#include <vector>
+#include <pstl/execution_impl.h>
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Hacking tbb" << std::endl;
+    tbb::parallel_invoke(
+                         []() { std::cout << "Hello " << std::endl; },
+                         []() {std::cout << "TBB!" << std::endl;}
+                         );
     return 0;
 }
