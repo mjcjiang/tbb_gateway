d78e51bfec7263019e2b86a2e9c4227fb00fba9b
mjcjiang <redmagic039@gmail.com>
2023-08-26 22:49:50 +0800

    code style change

--- apps/hacking_tbb.cc
+++ apps/hacking_tbb.cc
@@ -1,10 +1,11 @@
 #include <pstl/execution_impl.h>
 #include <tbb/tbb.h>
+
 #include <iostream>
 #include <vector>
 
 int main(int argc, char *argv[]) {
-  tbb::parallel_invoke([]() { std::cout << "Hello " << std::endl; },
-                       []() { std::cout << "TBB!" << std::endl; });
-  return 0;
+    tbb::parallel_invoke([]() { std::cout << "Hello " << std::endl; },
+                         []() { std::cout << "TBB!" << std::endl; });
+    return 0;
 }
